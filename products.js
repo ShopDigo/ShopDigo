b5e9b0f5a602af3101fd142746eeaa0f7b711795
ShopDigo <ishaann.yl@gmail.com>
2025-07-14 19:35:34 +0530

    Add files via upload

--- products.js
+++ products.js
@@ -1,6 +1,1 @@
-
-const products = [
-  { id: 1, name: "T-Shirt", price: 499 },
-  { id: 2, name: "Jeans", price: 999 },
-  { id: 3, name: "Jacket", price: 1499 }
-];
+// Product data from Google Sheets will be fetched here
--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-
-const container = document.getElementById("product-list");
-products.forEach(product => {
-  const div = document.createElement("div");
-  div.className = "product";
-  div.innerHTML = `
-    <h3>${product.name}</h3>
-    <p>₹${product.price}</p>
-    <button onclick="addToCart(${product.id})">Add to Cart</button>
-  `;
-  container.appendChild(div);
-});
-function addToCart(id) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.push(id);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert("Added to cart!");
-}
+// Animations, cart logic, and interactions
